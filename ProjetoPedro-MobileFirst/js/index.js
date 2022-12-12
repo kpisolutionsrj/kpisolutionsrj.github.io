0defb2eb5fd2fc031d9ee4a34ea58864feb24397
kpisolutionsrj <120418104+kpisolutionsrj@users.noreply.github.com>
2022-12-12 20:03:36 -0300

    Delete ProjetoPedro-MobileFirst directory

--- ProjetoPedro-MobileFirst/js/index.js
+++
@@ -1,8 +0,0 @@
-
-const botaoMenu = document.querySelector(".header-menu")
-const menu = document.querySelector(".navbar")
-
-botaoMenu.addEventListener('click', () =>{
-  menu.classList.toggle("navbar-menu--ativo")
-}
-)
